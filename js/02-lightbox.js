ee799e05ab4bcee089a1a65c997b599ed6323ac3
juliabykovaa <97822247+juliabykovaa@users.noreply.github.com>
2023-03-06 11:07:44 +0200

    done

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryBox = document.querySelector(".gallery")
+const imagesMarkup = createGalleryItems(galleryItems);
+galleryBox.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryItems(img) {
+    return img.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>  
+        </div>
+        `;
+    }).join('');
+}
+
+new SimpleLightbox('.gallery__link', {  captionsData: "alt", captionDelay: "250" });
+
